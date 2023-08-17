f2b76da8571754e32d90072fcb1280a99ad4fa41
Jorge Sánchez <48210515+jsanchezpc@users.noreply.github.com>
2023-08-17 17:41:16 +0000

    Update 3 files

--- src/components/BusModel.vue
+++ src/components/BusModel.vue
@@ -1,68 +1,74 @@
 <template>
-    <div class="app-container">
-        <div class="background"></div>
-        <div ref="canvasContainer" class="canvas-container"></div>
-    </div>
+  <div class="app-container">
+    <div class="background"></div>
+    <div ref="canvasContainer" class="canvas-container"></div>
+  </div>
 </template>
-  
+
 <script>
-import { ref, onMounted } from 'vue';
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import BusModel from '@/assets/3dmodels/bus/bus.gltf';
+import { ref, onMounted } from "vue";
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import BusModel from "@/assets/3dmodels/bus/bus.gltf";
 
 export default {
-    name: 'ThreeModelComponent',
-    setup() {
-        const canvasContainer = ref(null);
-        let scene, camera, renderer, model, light;
+  name: "ThreeModelComponent",
+  setup() {
+    const canvasContainer = ref(null);
+    let scene, camera, renderer, model, light;
 
-        onMounted(() => {
-            // Initialize Three.js
-            scene = new THREE.Scene();
-            camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
-            renderer = new THREE.WebGLRenderer();
-            renderer.setSize(window.innerWidth, window.innerHeight);
-            canvasContainer.value.appendChild(renderer.domElement);
+    onMounted(() => {
+      // Initialize Three.js
+      scene = new THREE.Scene();
+      camera = new THREE.PerspectiveCamera(
+        100,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
+      renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      canvasContainer.value.appendChild(renderer.domElement);
 
-            // Create a directional light
-            light = new THREE.DirectionalLight(0xffffff, 1); // White light with intensity 1
-            light.position.set(1, 1, 5); // Set light position
+      // Create a directional light
+      light = new THREE.DirectionalLight(0xffffff, 1); // White light with intensity 1
+      light.position.set(1, 1, 5); // Set light position
 
-            scene.add(light); // Add light to the scene
+      scene.add(light); // Add light to the scene
 
-            // Load model using GLTFLoader
-            const loader = new GLTFLoader();
-            loader.load(BusModel, (gltf) => {
-                model = gltf.scene;
-                scene.add(model);
-                console.log('buss added', gltf)
-            });
+      // Load model using GLTFLoader
+      const loader = new GLTFLoader();
+      loader.load(BusModel, (gltf) => {
+        model = gltf.scene;
+        scene.add(model);
+        console.log("buss added", gltf);
+      });
 
-            // Position the camera
-            camera.position.z = 7;
+      // Position the camera
+      camera.position.z = 7;
 
-            // Animation
-            const animate = () => {
-                requestAnimationFrame(animate);
-                if (model) {
-                    // model.rotation.x += 0.01;
-                    model.rotation.y += 0.01;
-                }
-                renderer.setClearColor(0xffffff, 0);
+      // Animation
+      const animate = () => {
+        requestAnimationFrame(animate);
+        if (model) {
+          // model.rotation.x += 0.01;
+          model.rotation.y += 0.01;
+          model.position.y = -2;
+        }
+        renderer.setClearColor(0xffffff, 0);
 
-                renderer.render(scene, camera);
-            };
-            animate();
-        });
+        renderer.render(scene, camera);
+      };
+      animate();
+    });
 
-        return {
-            canvasContainer,
-        };
-    },
+    return {
+      canvasContainer,
+    };
+  },
 };
 </script>
-  
+
 <style>
 .app-container {
     position: relative;
